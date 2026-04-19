2e6ba194b0ca384754c452f5767af72031aeaf02
dopey2 <dani.gabor1996@gmail.com>
2026-04-19 16:07:17 +0200

    track wezterm configuration

---
+++ wezterm/README.md
@@ -0,0 +1,20 @@
+
+## Setup
+
+Config lives in `wezterm/linux/wezterm.lua`. Symlink it into place:
+
+```sh
+ln -s ~/repos/dotfiles/wezterm/linux/wezterm.lua ~/.config/wezterm/wezterm.lua
+```
+
+---
+
+## Key bindings 
+
+| Key               | Action                                      |
+|-------------------|---------------------------------------------|
+| CTRL + SHIFT + E  | Split panel vertical (one left one right)   |
+| CTRL + SHIFT + O  | Split panel horizontal (one top one bottom) |
+| ALT + ARROW       | Panel navigation                            |
+| CTRL + LEFT/RIGHT | Jump to words                               |
+| HOME/END          | Jump to begin/end of the line               |
---
+++ wezterm/linux/wezterm.lua
@@ -0,0 +1,37 @@
+local wezterm = require 'wezterm'
+
+return {
+  default_prog = { '/bin/zsh' },
+
+  -- Font
+  font = wezterm.font('JetBrains Mono'),
+  font_size = 12,
+
+  -- Colors
+  color_scheme = 'Hacktober',
+&
+  -- Window
+  window_background_opacity = 0.95,
+  window_padding = {
+    left = 8,
+    right = 8,
+    top = 8,
+    bottom = 8,
+  },
+
+  keys = {
+    -- Pane splits
+    { key = 'E', mods = 'CTRL|SHIFT', action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' } },
+    { key = 'O', mods = 'CTRL|SHIFT', action = wezterm.action.SplitVertical   { domain = 'CurrentPaneDomain' } },
+
+    -- Pane navigation
+    { key = 'LeftArrow',  mods = 'ALT', action = wezterm.action.ActivatePaneDirection 'Left'  },
+    { key = 'RightArrow', mods = 'ALT', action = wezterm.action.ActivatePaneDirection 'Right' },
+    { key = 'UpArrow',    mods = 'ALT', action = wezterm.action.ActivatePaneDirection 'Up'    },
+    { key = 'DownArrow',  mods = 'ALT', action = wezterm.action.ActivatePaneDirection 'Down'  },
+
+    -- Word jump (explicit escape sequences for zsh reliability)
+    { key = 'LeftArrow',  mods = 'CTRL', action = wezterm.action.SendString '\x1b[1;5D' },
+    { key = 'RightArrow', mods = 'CTRL', action = wezterm.action.SendString '\x1b[1;5C' },
+  },
+}
