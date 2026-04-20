08a75ed22ecae8cb4fc9957bf11a75be7f3502e9
dopey2 <dani.gabor1996@gmail.com>
2026-04-20 20:35:01 +0200

    add a 'resource' alias to reload the .zshrc

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -4,3 +4,6 @@ alias wezterm='flatpak run org.wezfurlong.wezterm'
 # requires eza --> https://github.com/ogham/exa#installation
 alias ll='eza -ll'
 alias la='eza -la'
+
+# reload the zshrc
+alias resource='source ~/.zshrc && echo "zshrc reloaded !"'
