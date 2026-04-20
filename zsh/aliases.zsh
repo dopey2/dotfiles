35d045f751d0f6ac7959aedb2b0d8ad6ce47d058
dopey2 <dani.gabor1996@gmail.com>
2026-04-20 20:31:52 +0200

    fix aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,5 +2,5 @@
 alias wezterm='flatpak run org.wezfurlong.wezterm'
 
 # requires eza --> https://github.com/ogham/exa#installation
-alias ll=eza -ll
-alias la=eza -la
+alias ll='eza -ll'
+alias la='eza -la'
