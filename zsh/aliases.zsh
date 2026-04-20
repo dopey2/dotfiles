5f6f04539244e7ba92b5bfeee2118b84850464b4
dopey2 <dani.gabor1996@gmail.com>
2026-04-20 20:27:10 +0200

    add eza aliases alternative to ls

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,2 +1,6 @@
 #: wezterm — launch WezTerm (flatpak)
 alias wezterm='flatpak run org.wezfurlong.wezterm'
+
+# requires eza --> https://github.com/ogham/exa#installation
+alias ll=eza -ll
+alias la=eza -la
