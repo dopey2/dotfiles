43600809f1992c19ed091a754022963c89d43511
dopey2 <dani.gabor1996@gmail.com>
2026-04-20 21:00:33 +0200

    update git comments/echo

--- zsh/git.zsh
+++ zsh/git.zsh
@@ -3,16 +3,16 @@ git-current() {
   echo "$(git config --global user.name) <$(git config --global user.email)>"
 }
 
-#: git-profile-primary — switch to primary git account
+#: git-profile-primary — switch to primary git profile
 git-profile-primary() {
   git config --global user.name "$GIT_PRIMARY_NAME"
   git config --global user.email "$GIT_PRIMARY_EMAIL"
-  echo "Switched to personal git profile ($GIT_PRIMARY_NAME)"
+  echo "Switched to primary git profile ($GIT_PRIMARY_NAME)"
 }
 
-#: git-profile-secondary — switch to secondary git account
+#: git-profile-secondary — switch to secondary git profile
 git-profile-secondary() {
   git config --global user.name "$GIT_SECONDARY_NAME"
   git config --global user.email "$GIT_SECONDARY_EMAIL"
-  echo "Switched to work git profile ($GIT_SECONDARY_NAME)"
+  echo "Switched to secondary profile ($GIT_SECONDARY_NAME)"
 }
