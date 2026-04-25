61ed4830fd4a57fa6a71d0fcaf5276dedb497a85
dopey2 <dani.gabor1996@gmail.com>
2026-04-25 19:58:06 +0200

    update aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,8 +2,11 @@
 alias wezterm='flatpak run org.wezfurlong.wezterm'
 
 # requires eza --> https://github.com/ogham/exa#installation
+#: ll - like ls -l
+#: la - like ls -la
 alias ll='eza -ll'
 alias la='eza -la'
 
-# reload the zshrc
+
+#: resource - reload the zsrch
 alias resource='source ~/.zshrc && echo "zshrc reloaded !"'
