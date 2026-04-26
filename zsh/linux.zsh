1d9ca1b4cbacbdc9f8148a1f34eb7268ae5a68ca
dopey2 <dani.gabor1996@gmail.com>
2026-04-26 20:03:57 +0200

    add a copy command that copy to clipboard

--- zsh/linux.zsh
+++ zsh/linux.zsh
@@ -1,2 +1,11 @@
 #: open — open files/URLs with default app
 alias open='xdg-open'
+
+#: copy — copy to clipboard: `copy something` or `echo something | copy`
+copy() {
+  if [[ $# -gt 0 ]]; then
+    echo -n "$*" | xclip -selection clipboard
+  else
+    xclip -selection clipboard
+  fi
+}
